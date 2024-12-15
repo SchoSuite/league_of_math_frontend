653212b69e31964afa27633ceb9812f663e7eab3
BL-CZY <jiguangllsw@gmail.com>
2024-12-15 20:00:54 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import { io, Socket } from 'socket.io-client';
+
+	let socket: Socket | null;
+
+	onMount(() => {
+		socket = io('http://localhost:3000');
+
+		socket?.on('connect', () => {
+			console.log('Connection established');
+		});
+
+		socket?.on('disconnect', () => {
+			console.log('Connection ended');
+		});
+	});
+
+	onDestroy(() => {
+		socket?.disconnect();
+	});
+</script>
